43318aa63807622b2951ca600f6934ffc1a82bb2
Junaedih Alghazali <junaedih.alghazali1@gmail.com>
2023-02-24 16:13:11 +0700

    modify file

--- src/main/java/org/junaedih123/Main.java
+++ src/main/java/org/junaedih123/Main.java
@@ -1,4 +1,6 @@
-package org.junaedih123;public class Main {
+package org.junaedih123;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
